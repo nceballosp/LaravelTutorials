2e9c30d03557a76b30101c38a217ae29b6dbae7c
nceballosp <nataliaceballospos@gmail.com>
2025-02-21 20:01:10 -0500

    Tutorial 2 Done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,34 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Models\Product;
 use Illuminate\Http\RedirectResponse;
 
 class ProductController extends Controller
 {
-    public static $products = [
-        ["id"=>"1", "name"=>"TV", "description"=>"Best TV", "price"=>"290"],
-        ["id"=>"2", "name"=>"iPhone", "description"=>"Best iPhone", "price"=>"150"],
-        ["id"=>"3", "name"=>"Chromecast", "description"=>"Best Chromecast", "price"=>"99"],
-        ["id"=>"4", "name"=>"Glasses", "description"=>"Best Glasses", "price"=>"50"],
-    ];
-
     public function index(): View
     {
         $viewData = [];
         $viewData["title"] = "Products - Online Store";
         $viewData["subtitle"] =  "List of products";
-        $viewData["products"] = ProductController::$products;
+        $viewData["products"] = Product::all();
         return view('product.index')->with("viewData", $viewData);
     }
 
     public function show(string $id) : View | RedirectResponse
     {
-        if($id < 1 || $id > count(ProductController::$products)) {
+        if($id < 1 || $id > count(Product::all())) {
             return redirect()->route("home.index");
         }
         else {
             $viewData = [];
-            $product = ProductController::$products[$id-1];
+            $product = Product::findOrFail($id);
             $viewData["title"] = $product["name"]." - Online Store";
             $viewData["subtitle"] =  $product["name"]." - Product information";
             $viewData["product"] = $product;
@@ -47,16 +41,16 @@ class ProductController extends Controller
         return view('product.create')->with("viewData",$viewData);
     }
 
-    public function save(Request $request)
+    public function save(Request $request): RedirectResponse
     {
         $request->validate([
             "name" => "required",
             "price" => "required | gt:0"
         ]);
-        return redirect()->route("product.validation");
 
-        //dd($request->all());
-        //here will be the code to call the model and save it to the database
+        Product::create($request->only(["name","price"]));
+
+        return back();
     }
 
     public function validate() : View
