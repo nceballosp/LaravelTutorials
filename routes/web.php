93348816c43d0980aeb77a36abd2adbe62b2db6d
nceballosp <nataliaceballospos@gmail.com>
2025-02-26 22:33:00 -0500

    Tutorial 3 Done

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,9 @@ Route::get('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name("c
 Route::get('/cart/removeAll/', 'App\Http\Controllers\CartController@removeAll')->name("cart.removeAll");
 
 Auth::routes();
+
+Route::get('/image', 'App\Http\Controllers\ImageController@index')->name("image.index");
+Route::post('/image/save', 'App\Http\Controllers\ImageController@save')->name("image.save");
+
+Route::get('/image-not-di', 'App\Http\Controllers\ImageNotDIController@index')->name("imagenotdi.index");
+Route::post('/image-not-di/save', 'App\Http\Controllers\ImageNotDIController@save')->name("imagenotdi.save");
